0da41bb5d9ae6736d62e793f172e1e7cdeb40cf9
ss749236 <ss749236@gmail.com>
2023-08-06 20:38:05 +0530

    starting

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import React from 'react';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    getCoins();
+  }, []);
+  const APIURL = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false";
+  const getCoins = () => {
+    axios.get(APIURL).then((res) => {
+      console.log(res.data);
+      setCoins(res.data);
+    })
+  }
+
+  const handleChange = (event) => {
+    setSearch(event.target.value);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='search-box'>
+        <input type='search' placeholder='Find currency here' onChange={handleChange} />
+      </div>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Price</th>
+              <th>Symbol</th>
+              <th>Market_cap</th>
+              <th>Volumn</th>
+              <th>Image</th>
+              <th>Price_change</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              coins.map((coin) => {
+                return (
+                  <tr key={coin.id}>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                )
+              })
+            }
+          </tbody>
+        </table>
+      </div>
+
     </div>
   );
 }
